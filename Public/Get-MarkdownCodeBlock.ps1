7ff7fe5e01242dbb19a8e08350d693108dd8bf4c
dfinke <finked@hotmail.com>
2021-09-05 16:48:34 -0400

    Refactored to return md and blocks

--- Public/Get-MarkdownCodeBlock.ps1
+++ Public/Get-MarkdownCodeBlock.ps1
@@ -1,3 +1,89 @@
+function New-MardownEntry {
+    param(
+        [ValidateSet('markdown', 'psscript')]
+        $type,
+        $text
+    )
+
+    [pscustomobject]@{Type = $type; Text = @($text) }
+}
+
+function Get-PSScript {
+    param(
+        $parsedMarkdown
+    )
+
+    $parsedMarkdown | Where-Object type -eq 'psscript' | 
+    ForEach-Object { 
+        $end = $_.Text.Count - 2
+        $_.Text[1..$end]
+    }
+}
+
+function Update-MarkdownCodeFormatting {
+    param(
+        $parsedMarkdown
+    )
+
+    switch ($parsedMarkdown) {
+        { $_.Type -eq 'markdown' } {
+            continue 
+        }
+        { $_.Type -eq 'psscript' } {
+            $end = $_.Text.Count - 2
+            $s = $_.Text[1..$end] -join "`n"
+            $s = Invoke-Formatter -ScriptDefinition $s
+
+            $_.Text = "{0}`n{1}`n{2}" -f $_.Text[0], $s, $_.Text[-1]
+            continue 
+        }
+    }
+} 
+
+function Invoke-ParseMarkdown {
+    param(
+        [string[]]$markdown
+    )
+
+    $parsedMD = @()
+
+    $newMarkdowEntry = $true
+    $found = $false
+
+    switch ($markdown) {
+        { $_.Trim() -eq '```ps' -Or $_.Trim() -eq '```ps1' -Or $_.Trim() -eq '```powershell' } {
+            $parsedMD += New-MardownEntry "PSScript" ("{0}" -f $_)
+            $found = $true
+            continue
+        }
+    
+        { $_.StartsWith('```') } { 
+            $found = $false 
+            $parsedMD[-1].Text += "{0}" -f $_
+            $newMarkdowEntry = $true
+            continue
+        }
+    
+        default {
+            if ($found -eq $true) {
+                $parsedMD[-1].Text += "{0}" -f $_
+            }
+            else {
+                if ($newMarkdowEntry -eq $true) {
+                    $parsedMD += New-MardownEntry "Markdown"
+                    $newMarkdowEntry = $false                
+                }
+                $parsedMD[-1].Text += "{0}" -f $_
+            }
+        }
+    }
+
+    [PSCustomObject]@{
+        Path   = $Path
+        Script = (Get-PSScript $parsedMD)
+    }
+}
+
 function Get-MarkdownCodeBlock {
     <#
         .SYNOPSIS
@@ -49,30 +135,6 @@ function Get-MarkdownCodeBlock {
             $mdContent = $Path -split "`n"
         }
 
-        $script = $null
-        $found = $false
-        
-        switch ($mdContent) {
-            { $_.Trim() -eq '```ps' -Or $_.Trim() -eq '```ps1' -Or $_.Trim() -eq '```powershell' } {
-                $found = $true
-                continue
-            }
-            
-            { $_.StartsWith('```') } { 
-                $found = $false 
-                continue
-            }
-            
-            default {
-                if ($found -eq $true) {
-                    $script += "{0}`r`n" -f $_
-                }
-            }
-        }
-
-        [PSCustomObject]@{
-            Path   = $Path
-            Script = $script
-        }
+        Invoke-ParseMarkdown $mdContent
     }
 }
